a01dd0a00f7a6ff326683ae2d658fbb5d6a6a7df
Willy-557 <williamfsetiadi@gmail.com>
2026-03-23 10:18:36 +0700

    Making parent class (include getter setter, constructor, encapsulation)

---
+++ src/Person.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author William
+ */
+public class Person {
+    private String nama;
+    private int usia;
+
+    public Person(String nama, int usia) {
+        this.nama = nama;
+        this.usia = usia;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+
+    public void setUsia(int usia) {
+        this.usia = usia;
+    }
+    
+    
+}
